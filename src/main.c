7dcdc8de6b3b91f136f1577e3dbc8e1625a96a58
Hazzea Sunspot <hazzeasu@va-j5.kzn.21-school.ru>
2022-12-19 17:52:52 +0300

    commit 19.12

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ int main() {
   double orig_res = 10;
   int my_return = 0;
 
-  char str[255] = "2+(5 * 5 * 5 + 5 + 2 * 2)";
+  char str[255] = "2+(5 * 5 * 5 + 5 + 2 * 2) / (1 + 1)";
   // char str[255] = "7+(1*2*3+4+5*6)";
 
   my_return = s21_smart_calc(str, &my_res);
@@ -13,4 +13,3 @@ int main() {
   printf("my_res %lf, orig_res = %lf\n", my_res, orig_res);
   return 0;
 }
-sh
